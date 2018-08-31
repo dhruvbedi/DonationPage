d6d3db93dc4bbaa2f45fb9c9ce7ffec8044bdefa
Dhruv Bedi <dhruvbedi1996@gmail.com>
2018-08-31 15:43:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ function giveNow() {
     var text = input.innerHTML;
     var amountLeft = text-inputValue;
     var count = document.getElementById("count").innerHTML;
-    Total = parseInt(Total)+parseInt(inputValue);
-    console.log("Total Donations Raised = $"+Total);
+
     if(inputValue < 0 ){
       alert("Check the amount entered!");
       return;
     }
-    if((inputValue >= 1000 )|| (text == 0)){
+
+    Total = parseInt(Total)+parseInt(inputValue);
+    document.getElementById("total").innerHTML = Total;
+    var amountLeft = text-inputValue;
+    if((inputValue >= 1000 )|| (text <= 0) || (amountLeft < 0)){
       document.getElementById("donationLeft").innerHTML=0;
       document.getElementById("coloredBar").style.width = (1000)*0.4+"px";
       document.getElementById("count").innerHTML= ++count;
     }else{
-    var amountLeft = text-inputValue;
     document.getElementById("donationLeft").innerHTML=amountLeft;
     document.getElementById("coloredBar").style.width = (1000-amountLeft)*0.4+"px";
     document.getElementById("count").innerHTML= ++count;
